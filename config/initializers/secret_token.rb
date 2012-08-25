e939477ae6704a23e76c94163b9739dfe0236207
Gene Hallman <gene@flickme.com>
2012-08-25 14:24:25 -0700

    moved to rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Versus::Application.config.secret_token = '7e8a80b635f1c53add10eeca65ecd415443f1e82a8210ed1d792e80985719107632035094aa3f3b3298087331346ebf2d715c0020d017eda55504d28f9c76068'
